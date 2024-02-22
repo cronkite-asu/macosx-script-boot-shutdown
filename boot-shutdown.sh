9c43e332622e08141c850a3b6aed3cff8dfec8db
Jeremy Leggat <jleggat@asu.edu>
2024-02-22 12:49:58 -0700

    Update boot-shutdown.sh

--- boot-shutdown.sh
+++ boot-shutdown.sh
@@ -3,29 +3,41 @@
 #
 # Author: Vincenzo D'Amore v.damore@gmail.com
 # 20/11/2014
+# Updated: Jeremy Leggat
+# 20/02/2024
 #
+# Capture SIGTERM events to run commands before exit
+#
+# In MacOS the kernel sends to all user-space daemons SIGTERM at system shutdown
+# or reboot. Use this script to catch this signal and run commands at shutdown.
 
-function shutdown()
-{
+# The function that will get called when a SIGTERM signal is sent.
+# Put commands to run on shutdown here.
+function shutdown() {
   echo `date` " " `whoami` " Received a signal to shutdown"
 
-  # INSERT HERE THE COMMAND YOU WANT EXECUTE AT SHUTDOWN
+  launchctl unload /System/Library/LaunchDaemons/com.apple.xsan.plist
 
   exit 0
 }
 
-function startup()
-{
+# Initialize the script and then wait for a signal.
+# Put commands to run when starting here.
+function startup() {
   echo `date` " " `whoami` " Starting..."
-
-  # INSERT HERE THE COMMAND YOU WANT EXECUTE AT STARTUP
 
   tail -f /dev/null &
   wait $!
 }
 
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Catch TERM signals to run shutdown function
 trap shutdown SIGTERM
-trap shutdown SIGKILL
 
-startup;
-
+# Initialize and wait for TERM signal.
+startup
